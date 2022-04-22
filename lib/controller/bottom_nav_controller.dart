18e612e0414b602ac6f09b8e0d98a7d51d02d79e
Hussein M Elbhrway <husseinelbhrawy@gmail.com>
2022-04-22 21:32:43 +0200

    Build Bottom Nav Bar

---
+++ lib/controller/bottom_nav_controller.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome_icons.dart';
+import 'package:fluttericon/linearicons_free_icons.dart';
+import 'package:fluttericon/linecons_icons.dart';
+import 'package:get/get.dart';
+import 'package:store_app/views/screens/cart.dart';
+import 'package:store_app/views/screens/feeds.dart';
+import 'package:store_app/views/screens/home.dart';
+import 'package:store_app/views/screens/search.dart';
+import 'package:store_app/views/screens/user_info.dart';
+
+class BottomNavBarcontroller extends GetxController {
+  int currentIndex = 0;
+  int counter = 0;
+  final List<Map<String, dynamic>> screens = [
+    {'page': const HomeScreen(), 'appBarTitle': 'Home'},
+    {'page': const FeedsScreen(), 'appBarTitle': 'Feeds'},
+    {'page': const SearchScreen(), 'appBarTitle': 'Search'},
+    {'page': const CardScreen(), 'appBarTitle': 'Card'},
+    {'page': const UserInfoScreen(), 'appBarTitle': 'User Info'},
+  ];
+
+  final List<BottomNavigationBarItem> bottomNavBarItems = [
+    const BottomNavigationBarItem(
+      icon: Icon(LineariconsFree.home),
+      label: 'Home',
+      tooltip: 'Home',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(FontAwesome.rss),
+      label: 'Feeds',
+      tooltip: 'Feeds',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(null),
+      activeIcon: null,
+      backgroundColor: null,
+      label: 'Search',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.shopping_bag_outlined),
+      label: 'Cart',
+      tooltip: 'Cart',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Linecons.user),
+      label: 'User',
+      tooltip: 'User',
+    ),
+  ];
+
+  void bottomNavOnTao({required int newIndex}) {
+    currentIndex = newIndex;
+    counter++;
+    update();
+  }
+
+  void floatActionButtonOnPress() {
+    currentIndex = 2;
+    counter++;
+    update();
+  }
+}
---
+++ lib/views/widgets/bottom_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:fluttericon/linecons_icons.dart';
+import 'package:get/get.dart';
+import 'package:store_app/controller/bottom_nav_controller.dart';
+
+class BottomNavigationBarWidget extends StatelessWidget {
+  const BottomNavigationBarWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Get.put(BottomNavBarcontroller());
+    return GetBuilder(
+      builder: (BottomNavBarcontroller controller) {
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+                controller.screens[controller.currentIndex]['appBarTitle']),
+          ),
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: FloatingActionButton(
+              onPressed: () => controller.floatActionButtonOnPress(),
+              child: const Icon(Linecons.search),
+              backgroundColor: Colors.purple,
+              hoverElevation: 10,
+              splashColor: Colors.grey,
+              tooltip: 'Search',
+            ),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.miniCenterDocked,
+          body: controller.screens[controller.currentIndex]['page'],
+          bottomNavigationBar: BottomAppBar(
+            notchMargin: 8.0,
+            shape: const CircularNotchedRectangle(),
+            clipBehavior: Clip.antiAlias,
+            child: SizedBox(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  border: Border(
+                    top: BorderSide(
+                      color: Colors.grey,
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+                child: BottomNavigationBar(
+                  backgroundColor: Colors.white,
+                  unselectedItemColor: Colors.grey,
+                  selectedItemColor: Colors.purple,
+                  currentIndex: controller.currentIndex,
+                  elevation: 5.0,
+                  type: BottomNavigationBarType.shifting,
+                  onTap: (newIndex) =>
+                      controller.bottomNavOnTao(newIndex: newIndex),
+                  items: controller.bottomNavBarItems,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
